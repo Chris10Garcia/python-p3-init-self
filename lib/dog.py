db4c5bd74025d4247ae1c47a9e3848b78f48a651
Christian Garcia <bones192nd@gmail.com>
2023-06-01 15:53:28 -0400

    lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    # def __init__(self, name):
+    #     print(f"{name} is born!")
+        
+    def __init__ (self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+    
+    # def bark(self):
+    #     print("Woof!")
+
+    # def adopt(self, owner_name):
+    #     self.owner = owner_name
+    
+# clifford = Dog("clifford")
+
+# clifford.bark()
+# print(clifford.name)
+
